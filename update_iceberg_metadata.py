fd879e179328e6a9cc88ddfc2a571db16ea6c0ba
Christopher Sharkey <ev2900@gmail.com>
2024-10-16 08:59:17 -0600

    Update update_iceberg_metadata.py

--- update_iceberg_metadata.py
+++ update_iceberg_metadata.py
@@ -101,12 +101,49 @@ def update_json(bucket_name, object_key, old_bucket_name_or_path, new_bucket_nam
     
     # Upload the modified JSON back to S3
     s3.put_object(Bucket=bucket_name, Key=object_key, Body=modified_json_content)
+
+def find_most_recent_metadata_json(bucket_name, folder_path_to_metadata):
+    # Initialize a session using Amazon S3
+    s3 = boto3.client('s3')
     
+    # List objects in the specified folder
+    response = s3.list_objects_v2(Bucket = bucket_name, Prefix = folder_path_to_metadata)
+    
+    # Create an empty dictionary that will have each metadata.json file name and its last updated time
+    filename_lastupdate_dict = {}
+    
+    # Check if the response contains 'Contents' (i.e., objects exist)
+    if 'Contents' in response:
+        for obj in response['Contents']:
+            object_key = obj['Key']
+            
+            # Get the file extension
+            file_extension = object_key.split('.')[-1]
+            
+            if file_extension == 'json':
+                    # Read JSON file from S3
+                    response = s3.get_object(Bucket=bucket_name, Key=object_key)
+                    json_content = response['Body'].read().decode('utf-8')
+        
+                    # Load the JSON data into a Python object
+                    data = json.loads(json_content)
+                    
+                    filename_lastupdate_dict[object_key] = data['last-updated-ms']
+                    
+    # Find the most recent time (max value)
+    most_recent_time = max(filename_lastupdate_dict.values())
+
+    # Find the key corresponding to the most recent time
+    most_recent_event = [key for key, value in filename_lastupdate_dict.items() if value == most_recent_time][0]
+    
+    # Print the key-value pair
+    print(f"Use this path to in the Iceberg register command s3://{s3_bucket_name_w_metadata_to_update}/{most_recent_event}. This is the newest most recent metadata.json")
+
 # Adjust the values of these variables before running the script
-s3_bucket_name_w_metadata_to_update = '<s3 bucket name that has the Iceberg metadata that you want to update>' # ex. register-iceberg-2ut1suuihxyq 
-folder_path_to_metadata = '<path to the Iceberg metadata folder in the ^ bucket>' # ex. iceberg/iceberg.db/sampledataicebergtable/metadata/ 
-old_s3_bucket_name_or_path = '<name of S3 bucket or the S3 file path that you want to replace in the Iceberg metadata>' # ex. glue-iceberg-from-jars-s3bucket-2ut1suuihxyq
-new_s3_bucket_name_or_path = '<when you find an instance of ^ what you want to replace it with IE. the name of the S3 bucket or file path the metadata was moved to>' # ex. register-iceberg-2ut1suuihxyq
+s3_bucket_name_w_metadata_to_update = 'iceberg-update-metadata-s3-ja1hpqhh5sa9' # ex. register-iceberg-2ut1suuihxyq 
+folder_path_to_metadata = 'iceberg/iceberg.db/sampledataicebergtable/metadata/' # ex. iceberg/iceberg.db/sampledataicebergtable/metadata/ 
+old_s3_bucket_name_or_path = 'iceberg-s3-ezwbt8s0m3vd' # ex. glue-iceberg-from-jars-s3bucket-2ut1suuihxyq
+new_s3_bucket_name_or_path = 'iceberg-update-metadata-s3-ja1hpqhh5sa9' # ex. register-iceberg-2ut1suuihxyq
 
 # Initialize a session using Amazon S3
 s3 = boto3.client('s3')
@@ -116,11 +153,7 @@ response = s3.list_objects_v2(Bucket = s3_bucket_name_w_metadata_to_update, Pref
 
 # Check if the response contains 'Contents' (i.e., objects exist)
 if 'Contents' in response:
-    
-    # Sort objects before processing them
-    sorted_objects = sorted(response['Contents'], key=lambda obj: obj['LastModified'])
-    
-    for obj in sorted_objects['Contents']:
+    for obj in response['Contents']:
         object_key = obj['Key']
         
         # Get the file extension
@@ -128,7 +161,7 @@ if 'Contents' in response:
         
         if file_extension == 'avro':
             update_avro(s3_bucket_name_w_metadata_to_update, object_key, old_s3_bucket_name_or_path, new_s3_bucket_name_or_path)
-            
+        
         elif file_extension == 'json':
             update_json(s3_bucket_name_w_metadata_to_update, object_key, old_s3_bucket_name_or_path, new_s3_bucket_name_or_path)
             
@@ -136,3 +169,4 @@ else:
     print("No objects found in the specified folder")
     
 # Print the file name of the most recent metadata.json file. This can be used as an input for the register command
+find_most_recent_metadata_json(s3_bucket_name_w_metadata_to_update, folder_path_to_metadata)
