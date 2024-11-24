c482f87c31a726f23928e1e567b3eafe8e9e18c2
Christopher Sharkey <ev2900@gmail.com>
2024-11-23 17:26:51 -0700

    Update update_iceberg_metadata.py

--- update_iceberg_metadata.py
+++ update_iceberg_metadata.py
@@ -2,11 +2,12 @@ import boto3
 import io
 import fastavro
 import json
+import logging
+import sys
 
 def update_avro(bucket_name, object_key, old_bucket_name_or_path, new_bucket_name_or_path):
-    # Initialize the S3 client
-    s3 = boto3.client('s3')
-    
+    logger.info(f"Updating AVRO file {object_key.split('/')[-1]}")
+
     # Store the file as byte object
     response = s3.get_object(Bucket=bucket_name, Key=object_key)
 
@@ -59,10 +60,10 @@ def update_avro(bucket_name, object_key, old_bucket_name_or_path, new_bucket_nam
     
     # Upload the Avro file to S3
     s3.put_object(Bucket=bucket_name, Key=object_key, Body=output.getvalue())
+    logger.info(f"SUCCESSFULLY updated AVRO file {object_key.split('/')[-1]}")
 
 def update_json(bucket_name, object_key, old_bucket_name_or_path, new_bucket_name_or_path):
-    # Initialize the S3 client
-    s3 = boto3.client('s3')
+    logger.info(f"Updating JSON file {object_key.split('/')[-1]}")
     
     # Read JSON file from S3
     response = s3.get_object(Bucket=bucket_name, Key=object_key)
@@ -71,41 +72,32 @@ def update_json(bucket_name, object_key, old_bucket_name_or_path, new_bucket_nam
     # Load the JSON data into a Python object
     data = json.loads(json_content)
     
-    # Traverse and modify the JSON data
-    if isinstance(data, dict):
-        # Iterate through dictionary
-        for key in data:
-            if isinstance(data[key], str):
-                data[key] = data[key].replace(old_bucket_name_or_path, new_bucket_name_or_path)
-            elif isinstance(data[key], list):
-                for i, item in enumerate(data[key]):
-                    if isinstance(item, str):
-                        data[key][i] = item.replace(old_bucket_name_or_path, new_bucket_name_or_path)
-                    elif isinstance(item, dict):
-                        for sub_key in item:
-                            if isinstance(item[sub_key], str):
-                                item[sub_key] = item[sub_key].replace(old_bucket_name_or_path, new_bucket_name_or_path)
-                                
-    elif isinstance(data, list):
-        # Iterate through list
-        for i, item in enumerate(data):
-            if isinstance(item, str):
-                data[i] = item.replace(old_bucket_name_or_path, new_bucket_name_or_path)
-            elif isinstance(item, dict):
-                for key in item:
-                    if isinstance(item[key], str):
-                        item[key] = item[key].replace(old_bucket_name_or_path, new_bucket_name_or_path)
+    # Function to search and replace a value in the JSON structure
+    def replace_value(json_data, target_value, replacement_value):
+        if isinstance(json_data, dict):  # If it's a dictionary, iterate over the keys and values
+            for key, value in json_data.items():
+                if value == target_value:
+                    json_data[key] = replacement_value
+                else:
+                    replace_value(value, target_value, replacement_value)  # Recursively call for nested values
+        elif isinstance(json_data, list):  # If it's a list, iterate over the items
+            for index, item in enumerate(json_data):
+                if item == target_value:
+                    json_data[index] = replacement_value
+                else:
+                    replace_value(item, target_value, replacement_value)  # Recursively call for nested items
+
+    # Replace the old_bucket_name_or_path with the new_bucket_name_or_path
+    replace_value(data, old_bucket_name_or_path, new_bucket_name_or_path)
                                 
     # Convert the modified data back to JSON
     modified_json_content = json.dumps(data, indent = 4)
     
     # Upload the modified JSON back to S3
     s3.put_object(Bucket=bucket_name, Key=object_key, Body=modified_json_content)
+    logger.info(f"SUCCESSFULLY updated JSON file {object_key.split('/')[-1]}")
 
 def find_most_recent_metadata_json(bucket_name, folder_path_to_metadata):
-    # Initialize a session using Amazon S3
-    s3 = boto3.client('s3')
-    
     # List objects in the specified folder
     response = s3.list_objects_v2(Bucket = bucket_name, Prefix = folder_path_to_metadata)
     
@@ -137,13 +129,21 @@ def find_most_recent_metadata_json(bucket_name, folder_path_to_metadata):
     most_recent_event = [key for key, value in filename_lastupdate_dict.items() if value == most_recent_time][0]
     
     # Print the key-value pair
-    print(f"Use this path to in the Iceberg register command s3://{s3_bucket_name_w_metadata_to_update}/{most_recent_event}. This is the newest most recent metadata.json")
+    logger.info(f"Use this path to in the Iceberg register command s3://{s3_bucket_name_w_metadata_to_update}/{most_recent_event}. This is the newest most recent metadata.json")
 
 # Adjust the values of these variables before running the script
 s3_bucket_name_w_metadata_to_update = '<s3 bucket name that has the Iceberg metadata that you want to update>' # ex. register-iceberg-2ut1suuihxyq 
 folder_path_to_metadata = '<path to the Iceberg metadata folder in the ^ bucket>' # ex. iceberg/iceberg.db/sampledataicebergtable/metadata/ 
 old_s3_bucket_name_or_path = '<name of S3 bucket or the S3 file path that you want to replace in the Iceberg metadata>' # ex. glue-iceberg-from-jars-s3bucket-2ut1suuihxyq
 new_s3_bucket_name_or_path = '<when you find an instance of ^ what you want to replace it with IE. the name of the S3 bucket or file path the metadata was moved to>' # ex. register-iceberg-2ut1suuihxyq
+
+# Set up the logger with CloudWatch
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)  # Logs will appear in the CloudWatch Logs stream
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler) # Add the handler to the logger
 
 # Initialize a session using Amazon S3
 s3 = boto3.client('s3')
@@ -166,7 +166,7 @@ if 'Contents' in response:
             update_json(s3_bucket_name_w_metadata_to_update, object_key, old_s3_bucket_name_or_path, new_s3_bucket_name_or_path)
             
 else:
-    print("No objects found in the specified folder")
+    raise Exception(f"No objects found in s3://{s3_bucket_name_w_metadata_to_update}/{folder_path_to_metadata}")
     
 # Print the file name of the most recent metadata.json file. This can be used as an input for the register command
 find_most_recent_metadata_json(s3_bucket_name_w_metadata_to_update, folder_path_to_metadata)
